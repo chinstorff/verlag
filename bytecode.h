8c8c6e9d05fbd343f6224adfd42c592ad2d51aae
Christopher Hinstorff <chris@chinstorff.com>
2014-08-06 12:01:13 -0700

    add more bytecodes

--- bytecode.h
+++ bytecode.h
@@ -1,7 +1,18 @@
 enum {
   HALT,
   ICONST,
-  PRINT,
+  PRINT, 
   IADD,
   ISUB,
+  IMUL,
+  ILT,     // int less than
+  IEQ,     // int equal
+  BR,      // branch
+  BRT,     // branch if true
+  BRF,     // branch if false
+  LOAD,    // load from local
+  GLOAD,   // load from global
+  STORE,   // store in local
+  GSTORE,  // store in global
+  POP,
 };
